b4262a57e939db48fcb2bb41401a6355ac5ddfe0
Clyde Lartey <dclartey@outlook.com>
2022-08-12 01:37:52 -0700

    Initial Commit

---
+++ SummerHolidaysPractice/SummerHolidaysPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummerHolidaysPracticeApp.swift
+//  SummerHolidaysPractice
+//
+//  Created by Clyde Lartey on 12/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SummerHolidaysPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
